d21564a32f62cc90e8fa7bcb8459f215c4740ef2
Fathia Nuq Qamarina <74334625+FathiaNuqQamarina@users.noreply.github.com>
2022-10-27 19:23:34 +0800

    posttest 6 Web

---
+++ Posttest_website/admin/adminlog.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+</head>
+<body>
+    <h1>Login Sebagai Admin</h1>
+    <form method="" action="admin/data_admin.html">
+    <table>
+            <tr>
+                <td><label for="username">Username</label></td>
+                <td><input type="text" name="username" class="inputan" placeholder="Username" required></td>
+            </tr>
+            <tr>
+                <td><label for="password">Password</label></td>
+                <td><input type="password" name="password" class="inputan" placeholder="Password" required></td>
+            </tr>
+            <tr>
+                <td><button name="Login"><a href="admin/data_admin.html"></a>Login</button></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
